4d2fc748439289515fb7384f8774e514a60eaa62
rickypcyt <rickypcyt@gmail.com>
2024-06-12 16:34:04 +0200

    Hello World Test

---
+++ cmake-build-debug/CMakeFiles/3DDonut.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "3DDonut"
+  "3DDonut.pdb"
+  "CMakeFiles/3DDonut.dir/main.cpp.o"
+  "CMakeFiles/3DDonut.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3DDonut.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/3DDonut.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for 3DDonut.
+# This may be replaced when dependencies are built.
